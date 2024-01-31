d5baa5542e1303800be0db280497196355492031
ashwindhevaraj <ashwindhevaraj@gmail.com>
2024-01-31 17:40:29 +0530

    csvreaddone

--- studentrecord.py
+++ studentrecord.py
@@ -1,5 +1,6 @@
 import pickle
 import os
+import csv
 class student:
     def adddata(self):
         f1=open("aswintext.dat","wb+")
@@ -22,9 +23,31 @@ class student:
                     break
         except:
             f1.close()
-        
+    def csvwrite(self):
+        f1=open("test.csv","w+",newline="")
+        wob=csv.writer(f1)
+        n=int(input('enter no of records'))
+        for i in range(n):
+            username=input('enter your username')
+            pword=input('enter your password')
+            a=[username,pword]
+            wob.writerow(a)
+        f1.close()
+    def csvread(self,text):
+        f1=open('test.csv','r')
+        rob=csv.reader(f1)
+        for i in rob:
+            if i[0]==text:
+                print("password is ",i[1])
+                break
+            else:
+                print("record not present")
+
 
 s1=student()
-s1.adddata()
-s1.searchdata()
+#s1.adddata()
+#s1.searchdata()
+s1.csvwrite()
+s1.csvread("1111")
+
 
